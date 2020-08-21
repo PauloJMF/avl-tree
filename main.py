68c99731c7e5d07aa91a55cfb5a5ccddcf37abff
João <jgckruger@gmail.com>
2020-08-21 18:34:52 -0300

    New main

--- main.py
+++ main.py
@@ -2,33 +2,18 @@ from classes.tree import Tree
 import sys
 
 tree = Tree()
+tree.visit_preorder()
+
+for i in range(10):
+    print(i)
+    tree.insert(i)
+tree.visit_inorder()
+tree
+#tree.visit_preorder()
 #tree.visit_preorder()
 #tree.visit_inorder()
 #tree.visit_postorder()
-tree.print_preorder()
 #tree.print_inorder()
 #tree.print_postorder()
-
-try:
-    filepath = sys.argv[1]
-except:
-    filepath = 'test_input.txt'
-
-final_translate = ""
-
-with open(filepath) as fp:
-    line = fp.readline()
-    while line:
-        letters = line.split(' ')
-        morse_codes = []
-        for letter in letters:
-            morse, caminho = tree.find(letter.strip())
-            print("Caminho letra {}: {}".format(morse, " -> ".join(caminho)))
-            morse_codes.append(morse)
-        morse_word = ''.join(morse_codes)
-        final_translate += morse_word + " "
-        line = fp.readline()
-
-print(final_translate)
 
 
